32578701ee11fbde53a45c3cf5f22c778c2e8da1
Atul2330 <singh.atul5914@gmail.com>
2025-04-04 13:17:08 +0530

    Fixed an issue which didnt let me add new items

--- src/App.jsx
+++ src/App.jsx
@@ -1,78 +1,92 @@
 import { useState } from "react";
+import { TodoForm } from "./components/TodoForm/TodoForm";
+import { TodoList } from "./components/TodoList/TodoList";
 import styles from "./App.module.css";
-import ToDoForm from "./Component/ToDoForm/ToDoForm";
-import ToDoList from "./Component/ToDoList/ToDoList";
-import ToDoFilters from "./Component/ToDoFilters/ToDoFilters";
+import { TodoFilters } from "./components/TodoFilters/TodoFilters";
 
-
-const TODO_DEFAULT=[{
-  id:"1",
-  name:"Charge all the required electronics",
-  description:"Charge your PowerBank, Phone, Laptop",
-  deadline:"2025-04-01",
-  priority:"low",
-  completed:true,
-},
-{
-  id:"2",
-  name:"Complete React Project",
-  description:"Complete two React Projects you are working on and push it to Git",
-  deadline:"2025-04-10",
-  priority:"high",
-  completed:false,
-},
-{
-  id:"3",
-  name:"Start Preparing for Interviews",
-  description:"Start the JavaScript Course and create note that will be helpful in interviews",
-  deadline:"2025-04-30",
-  priority:"medium",
-  completed:true,
-},
-{
-  id:"4",
-  name:"Test with Name only",
-  description:"",
-  deadline:"",
-  priority:"none",
-  completed:false
-}
-]
+const TODOS_DEFAULT = [
+  {
+    id: "1",
+    name: "Buy an Ice Cream",
+    description: "The white one with chocolate",
+    deadline: "2025-02-09",
+    priority: "low",
+    completed: false,
+  },
+  {
+    id: "2",
+    name: "Sell old MacBook Pro 2025",
+    description: "Try to sell it on OLX",
+    deadline: "2025-02-28",
+    priority: "high",
+    completed: false,
+  },
+  {
+    id: "3",
+    name: "Charge Powerbank",
+    description: "For the next travelling",
+    deadline: "2025-02-15",
+    priority: "medium",
+    completed: true,
+  },
+  {
+    id: "4",
+    name: "Test Todo onlye with a name",
+    description: "",
+    deadline: "",
+    priority: "none",
+    completed: false,
+  },
+];
 
 function App() {
-const [todos,setTodos]=useState(TODO_DEFAULT);
-const [filters,setFilters]=useState({});
-function handleCreate(newTodo){
-  setTodos((prevTodos)=>[
-    ...prevTodos,
-    {id: `${prevTodos.length+1}`, newTodo}])
-}
+  const [todos, setTodos] = useState(TODOS_DEFAULT);
+  const [filters, setFilters] = useState({});
 
-function handleUpdate(id,newTodo){
-  setTodos((prevTodos)=> prevTodos.map((todo)=> todo.id === id ?newTodo :todo))
-}
-function filterTodos(todo){
-  const {completed,priority}=filters
-  return(
-  (completed===""||todo.completed===completed) && (priority===""|| todo.priority===priority)
+  function handleCreate(newTodo) {
+    setTodos((prevTodos) => [
+      ...prevTodos,
+      { id: `${prevTodos.length + 1}`, ...newTodo },
+    ]);
+  }
+
+  function handleUpdate(id, newTodo) {
+    setTodos((prevTodos) =>
+      prevTodos.map((todo) => (todo.id === id ? newTodo : todo))
+    );
+  }
+
+  function handleDelete(id) {
+    setTodos((prevTodos) => prevTodos.filter((todo) => todo.id !== id));
+  }
+
+  function filterTodos(todo) {
+    const { completed, priority } = filters;
+
+    return (
+      (completed === "" || todo.completed === completed) &&
+      (priority === "" || todo.priority === priority)
+    );
+  }
+
+  return (
+    <div className={styles.App}>
+      <header className={styles.Header}>
+        <img className={styles.Logo} src="/to-do.png" />
+        <h2 className={styles.Title}>To-Do App</h2>
+      </header>
+
+      <div className={styles.AppContainer}>
+        <TodoForm onCreate={handleCreate} />
+        <TodoFilters onFilter={setFilters} />
+        <TodoList
+          todos={todos.filter(filterTodos)}
+          onUpdate={handleUpdate}
+          onDelete={handleDelete}
+        />
+      </div>
+    </div>
   );
 }
-function handleDelete(id){
-  setTodos((prevTodos)=>prevTodos.filter((todo)=> todo.id !==id))
-}
-  return(
-  <div className={styles.App}>
-    <header className={styles.Header}>
-      <img className={styles.Logo} src="/to-do.png" />
-      <h2 className={styles.Title}>To-Do-App</h2>
-    </header>
-    <div className={styles.AppContainer}>
-      <ToDoForm onCreate={handleCreate}/>
-      <ToDoFilters onFilter={setFilters}/>
-      <ToDoList todos={todos.filter(filterTodos)} onUpdate={handleUpdate} onDelete={handleDelete}/>
-    </div>
-  </div>
-  )
-}
 
-export default App
+export default App;
--- src/Constants/priorities.js
+++ src/Constants/priorities.js
@@ -1,8 +1,8 @@
-export const PRIORITIES={
-    none:{label:"None", color:""},
-    low:{label:"Low", color:"Teal"},
-    medium:{label:"Medium", color:"Orange"},
-    high:{label:"High", color:"Tomato"},
+export const PRIORITIES = {
+  none: { label: "None", color: "" },
+  low: { label: "Low", color: "Teal" },
+  medium: { label: "Medium", color: "Gold" },
+  high: { label: "High", color: "Salmon" },
 };
 
-export const PRIORITY_DEFAULT="none";
+export const PRIORITY_DEFAULT = "none";
